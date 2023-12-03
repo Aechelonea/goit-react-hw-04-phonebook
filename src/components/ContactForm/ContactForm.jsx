b18e1893a0e83f8ff42a916c73da12ff91e7a239
Aechelonea <judyta.patoka@gmail.com>
2023-12-03 23:15:53 +0100

    Task done

---
+++ src/components/ContactForm/ContactForm.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { nanoid } from 'nanoid';
+import PropTypes from 'prop-types';
+import styles from './ContactForm.module.css';
+
+const ContactForm = ({ onAddContact }) => {
+  const [name, setName] = useState('');
+  const [number, setNumber] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onAddContact({ id: nanoid(), name, number });
+    setName('');
+    setNumber('');
+  };
+
+  return (
+    <form onSubmit={handleSubmit} className={styles.contactForm}>
+      <label>
+        Name:
+        <input
+          type="text"
+          name="name"
+          pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+          title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+          required
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </label>
+      <label>
+        Number:
+        <input
+          type="tel"
+          name="number"
+          pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+          title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+          required
+          value={number}
+          onChange={(e) => setNumber(e.target.value)}
+        />
+      </label>
+      <button type="submit">Add contact</button>
+    </form>
+  );
+};
+
+ContactForm.propTypes = {
+  onAddContact: PropTypes.func.isRequired,
+};
+
+export default ContactForm;
---
+++ src/components/ContactListItem/ContactListItem.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import styles from './ContactListItem.module.css';
+
+const ContactListItem = ({ contact, onDelete }) => {
+  return (
+    <li className={styles.contactListItem}>
+      {contact.name}: {contact.number}
+      <button onClick={onDelete}>Delete</button>
+    </li>
+  );
+};
+
+ContactListItem.propTypes = {
+  contact: PropTypes.shape({
+    name: PropTypes.string.isRequired,
+    number: PropTypes.string.isRequired,
+  }).isRequired,
+  onDelete: PropTypes.func.isRequired,
+};
+
+export default ContactListItem;
